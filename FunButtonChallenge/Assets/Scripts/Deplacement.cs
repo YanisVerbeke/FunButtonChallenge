412935c58f90ea425da15bd9a936f1750ad9f4bd
YanisVerbeke <yanis88357@gmail.com>
2020-10-23 01:05:26 +0200

    Add Button, Deplacement, Acceleration

---
+++ FunButtonChallenge/Assets/Scripts/Deplacement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    [SerializeField]
+    int speed;
+    public Vector3 previousVel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(speed, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -260)
+        {
+            //
+        }
+        if (transform.position.x > 260)
+        {
+            //
+            
+        }
+    }
+
+
+}
---
+++ FunButtonChallenge/Assets/Scripts/FunHandler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class FunHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool pointerDown;
+    private float pointerDownTimer;
+
+    public float requireHoldTime;
+
+    [SerializeField]
+    GameObject sphere;
+    Deplacement sphereScript;
+    Vector3 spherePreviousVel;
+    bool previousVelStocked = false;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pointerDown = true;
+        //Debug.Log("Down");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Reset();
+        //Debug.Log("Up");
+    }
+    void Update()
+    {
+        if (pointerDown)
+        {
+            pointerDownTimer += Time.deltaTime;
+            if (pointerDownTimer >= requireHoldTime)
+            {
+                Reset();
+            }
+            Debug.Log("Timer : " + pointerDownTimer);
+            StopBall();
+        }
+    }
+
+    public void StopBall()
+    {
+        sphereScript = sphere.GetComponent<Deplacement>();
+        if (!previousVelStocked)
+        {
+            sphereScript.previousVel = sphereScript.rigidbody.velocity;
+            previousVelStocked = true;
+            Debug.Log(sphereScript.previousVel);
+        }
+        sphereScript.rigidbody.velocity = Vector3.zero;
+        sphereScript.previousVel *= 1.002f;
+    }
+
+    private void Reset()
+    {
+        previousVelStocked = false;
+        pointerDown = false;
+        pointerDownTimer = 0;
+        sphereScript.rigidbody.velocity = sphereScript.previousVel;
+    }
+}
